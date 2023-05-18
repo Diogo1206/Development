5d1c1ab15406ed1d568827c554c46bff331fd2bf
Diogo1206 <diogof_dasilva@hotmail.com>
2023-05-18 14:49:18 -0300

    Curso Udemy até a aula 31

---
+++ Udemy/JavaScript/Inicio/desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+let raio = 10
+const PI = 3.141592
+let areaCirc = PI*raio*raio
+
+console.log(PI * raio * raio + " m²")
+console.log(areaCirc + " m²")
+
+areaCirc = (Math.PI*raio*raio)
+console.log(areaCirc + " m²")
---
+++ Udemy/JavaScript/Inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+let a = 7
+let b = 94
+
+let temp = b
+b = a
+a = temp
+
+//[a, b] = [b, a]
+
+console.log("A = " + a)
+console.log("B = " + b)
+
+// depois da troca... a = 94 e b = 7
